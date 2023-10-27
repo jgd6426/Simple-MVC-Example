bfb4e4f7900a1a20a0cbdf5f44969ef5d80c2f3d
Judy Derrick <jgd6426@rit.edu>
2023-10-27 14:05:53 -0400

    during class
    i dont think my database is working

--- server/app.js
+++ server/app.js
@@ -10,6 +10,13 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/simpleMVCExample';
+mongoose.connect(dbURI).catch(err => {
+  if(err) {
+    console.log('Could not connect to database');
+    throw(err);
+  }
+});
 
 const app = express();
 
